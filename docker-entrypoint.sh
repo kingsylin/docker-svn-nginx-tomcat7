afdc8f8de2b355c6b2783d4d849dd666b6d40822
kingsylin <kingsylin@vip.qq.com>
2017-03-21 14:22:01 +0800

    udpate

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,21 +2,23 @@
 
 set -x
 
-app_name="${APP_NAME:-project}"
+app_name="${APP_NAME:-default}"
 build_file="${BUILD_FILE:-ROOT.war}"
 build_cmd="${BUILD_CMD:-clean package -Dmaven.test.skip=true}"
 
-echo "yes" | svn co --username=${SVN_USER} --password=${SVN_PWD}  ${SVN_URL} ${app_name}
+mkdir project
 
-cd ${app_name}
+echo "yes" | svn co --username=${SVN_USER} --password=${SVN_PWD}  ${SVN_URL} project/${app_name}
 
-../maven/bin/mvn ${build_cmd}
+cd  project/${app_name}
 
-cd ../
+../../maven/bin/mvn ${build_cmd}
+
+cd ../../
 
 mv tomcat/webapps/ROOT tomcat/webapps/ROOT.bak
 
-cp project/target/${build_file} tomcat/webapps
+cp project/${app_name}/target/${build_file} tomcat/webapps
 
 chmod +x tomcat/bin/catalina.sh
 
